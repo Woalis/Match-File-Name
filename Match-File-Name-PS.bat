792a07c4bc8dd9077f01ddd5dbd6ce8b91933893
Woalis <93735822+Woalis@users.noreply.github.com>
2025-06-01 15:16:23 -0400

    Update Match-File-Name-PS.bat with license

--- Match-File-Name-PS.bat
+++ Match-File-Name-PS.bat
@@ -1,3 +1,6 @@
+REM Source https://blog.danskingdom.com/allow-others-to-run-your-powershell-scripts-from-a-batch-file-they-will-love-you-for-it/
+REM https://blog.danskingdom.com/about/#-license
+REM https://creativecommons.org/licenses/by/4.0/
 @ECHO OFF
 SET ThisScriptsDirectory=%~dp0
 SET PowerShellScriptPath=%ThisScriptsDirectory%Match-File-Name-PS.ps1
